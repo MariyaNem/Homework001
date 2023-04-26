9faa43838fd55241825b6b8926e7bab9b07e2865
Mariya Nemzorova <nemzorova777@mail.ru>
2023-04-27 01:23:03 +0700

    solved task4

--- src/main/java/task1/Tasks.java
+++ src/main/java/task1/Tasks.java
@@ -1,6 +1,9 @@
 package task1;
+import java.util.*;
+import java.text.*;
 
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class Tasks {
@@ -9,6 +12,7 @@ public class Tasks {
 //        ts1();
 //        ts2();
 //        ts3();
+        ts4();
     }
 
 
@@ -22,30 +26,30 @@ public class Tasks {
         int initialValue = in.nextInt();
 
         int[] arr = new int[len];
-        for (int i = 0; i <= arr.length-1; i++){
+        for (int i = 0; i <= arr.length - 1; i++) {
             arr[i] = initialValue;
             System.out.printf("%d ", arr[i]);
         }
     }
 
-//2. Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
+//2. Задать одномерный массив и найти в нем минимальный и максимальный элементы;
 
     private static void ts2() {
-        int[] arr = new int[] {3,5,6,1,4};
+        int[] arr = new int[]{3, 5, 6, 1, 4};
         int max = arr[0];
         int min = arr[0];
-        for (int i = 0; i <= arr.length-1; i++){
-            if (arr[i] > max){
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] > max) {
                 max = arr[i];
             }
-            if (arr[i] < min){
+            if (arr[i] < min) {
                 min = arr[i];
             }
         }
         System.out.println("max = " + max + "; min = " + min);
     }
 
-//    4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью
+    //    3. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью
 //    цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно).
 //    Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны,
 //    то есть [0][0], [1][1], [2][2], …, [n][n];
@@ -62,6 +66,35 @@ public class Tasks {
                 System.out.printf("%d", arr[i][j]);
             }
             System.out.println();
+        }
+    }
+
+//4. В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида:
+//"Доброе утро, <Имя>!", если время от 05:00 до 11:59
+//"Добрый день, <Имя>!", если время от 12:00 до 17:59;
+//"Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+//"Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+
+    private static void ts4() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = in.nextLine();
+
+
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("hh");
+        int time = Integer.parseInt(formatForDateNow.format(dateNow));
+        if (time >= 5 && time < 12){
+            System.out.printf("Доброе утро, %s!%n", name);
+        }
+        if (time >= 12 && time < 18) {
+            System.out.printf("Добрый день, %s!%n", name);
+        }
+        if (time >= 18 && time < 23) {
+            System.out.printf("Добрый вечер, %s!%n", name);
+        }
+        if (time >= 23 || time < 5) {
+            System.out.printf("Доброй ночи, %s!%n", name);
         }
     }
 }
